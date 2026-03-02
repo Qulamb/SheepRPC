170498205072fd104cba20571752fb44bb4e6a50
quxiaoyang <554382396@qq.com>
2026-03-02 23:30:26 +0800

    新增channel池化功能，避免频繁TCP握手开销

--- src/main/java/com/xiaoyang/Client/netty/NettyRPCClient.java
+++ src/main/java/com/xiaoyang/Client/netty/NettyRPCClient.java
@@ -7,7 +7,6 @@ import com.xiaoyang.Client.serviceCenter.ServiceCenter;
 import com.xiaoyang.Client.serviceCenter.ZkServiceCenter;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -17,53 +16,72 @@ import java.net.InetSocketAddress;
 
 /**
  * 实现RPCClient接口
+ * 使用Channel连接池技术，避免频繁TCP握手开销
  */
 public class NettyRPCClient implements RPCClient {
     private static final Bootstrap bootstrap;
     private static final EventLoopGroup eventLoopGroup;
-    private String host;
-    private int port;
+    private static final ChannelPoolManager poolManager;
     private ServiceCenter serviceCenter;
-    public NettyRPCClient(String host, int port) {
-        this.host = host;
-        this.port = port;
+
+    public NettyRPCClient(String host, int port) throws InterruptedException {
+        this.serviceCenter = new ZkServiceCenter();
     }
+
     public NettyRPCClient() throws InterruptedException {
-        this.serviceCenter =new ZkServiceCenter();
+        this.serviceCenter = new ZkServiceCenter();
     }
+
     // netty客户端初始化，重复使用
     static {
         eventLoopGroup = new NioEventLoopGroup();
         bootstrap = new Bootstrap();
-        bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class)
+        bootstrap.group(eventLoopGroup)
+                .channel(NioSocketChannel.class)
                 .handler(new NettyClientInitializer());
+        // 初始化连接池管理器，最大连接数10
+        poolManager = new ChannelPoolManager(bootstrap, 10);
     }
 
     /**
-     * 这里需要操作一下，因为netty的传输都是异步的，你发送request，会立刻返回， 而不是想要的相应的response
+     * 使用连接池发送请求
+     * 从连接池获取Channel，避免频繁TCP握手
      */
     @Override
     public RPCResponse sendRequest(RPCRequest request) {
         InetSocketAddress address = serviceCenter.serviceDiscovery(request.getInterfaceName());
-        host=address.getHostName();
-        port=address.getPort();
+        Channel channel = null;
+
         try {
-            ChannelFuture channelFuture  = bootstrap.connect(host, port).sync();
-            Channel channel = channelFuture.channel();
+            // 从连接池获取Channel
+            channel = poolManager.acquireChannel(address);
+
             // 发送数据
             channel.writeAndFlush(request);
             channel.closeFuture().sync();
-            // 阻塞的获得结果，通过给channel设计别名，获取特定名字下的channel中的内容（这个在hanlder中设置）
-            // AttributeKey是，线程隔离的，不会由线程安全问题。
-            // 实际上不应通过阻塞，可通过回调函数
+
+            // 阻塞的获得结果，通过给channel设计别名，获取特定名字下的channel中的内容
             AttributeKey<RPCResponse> key = AttributeKey.valueOf("RPCResponse");
             RPCResponse response = channel.attr(key).get();
 
             System.out.println(response);
             return response;
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            // 释放Channel回连接池
+            if (channel != null) {
+                poolManager.releaseChannel(address, channel);
+            }
         }
         return null;
+    }
+
+    /**
+     * 关闭连接池和EventLoopGroup
+     */
+    public void close() {
+        poolManager.closeAll();
+        eventLoopGroup.shutdownGracefully();
     }
 }
